7586219843a75bb783f1575a1475d6dabff33581
Raisess <raisechif@gmail.com>
2022-12-22 13:04:00 -0300

    commands/run_migrations_command: execute migration

--- src/commands/run_migrations_command.py
+++ src/commands/run_migrations_command.py
@@ -1,3 +1,5 @@
+import os
+
 from commands.abs_command import AbstractCommand
 from database.abs_database import DatabaseConnectionOpts
 from database.database_factory import DatabaseFactory
@@ -13,12 +15,19 @@ class RunMigrationsCommand(AbstractCommand):
       raise Exception(f"Invalid arguments, database name not provided")
 
     database_name = args[0]
-    opts = DatabaseConnectionOpts("localhost", "user", "pass", 5432)
+    opts = DatabaseConnectionOpts(
+      os.getenv("DB_NAME") or database_name,
+      os.getenv("DB_HOST") or "localhost",
+      os.getenv("DB_USER") or "root",
+      os.getenv("DB_PASS"),
+      os.getenv("DB_PORT") or 5432
+    )
     database = DatabaseFactory.Init(database_name, opts)
 
     files = self.__file_manager.list()
     if not len(files):
       raise Exception("No migrations found")
 
+    files.sort()
     for file in files:
-      print(self.__file_manager.read(file))
+      database.query(self.__file_manager.read(file))
